4d01916fcb9ba1aa66b7ad20b1385742bfb14b99
Andreas Kellas <andreas.kellas@columbia.edu>
2024-09-16 19:47:29 -0400

    Create script for taking the union of model traces

---
+++ scripts/modelunion.py
@@ -0,0 +1,55 @@
+"""
+Given a list of model traces structured as JSON files, output the union of all
+of the traces as a JSON file.
+"""
+
+import json
+import argparse
+
+def model_union(models):
+    all_globals = set()
+    all_reduces = set()
+
+    for obj in models:
+        all_globals.update(obj.get("globals", []))
+        all_reduces.update(obj.get("reduces", []))
+
+    result = {
+        "globals": list(all_globals),
+        "reduces": list(all_reduces)
+    }
+
+    return result
+
+def main():
+
+    parser = argparse.ArgumentParser(
+        description=(
+            "Process a list of JSON files and compute union of "
+            "GLOBALS and REDUCES fields."))
+
+    # Add argument to accept a list of file names
+    parser.add_argument(
+        'filenames',
+        metavar='F',
+        type=str,
+        nargs='+',
+        help='JSON files to be processed')
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    json_objects = []
+
+    # Read each file and load it as a JSON object
+    for filename in args.filenames:
+        with open(filename, 'r', encoding='utf-8') as fd:
+            json_data = json.load(fd)
+            json_objects.append(json_data)
+
+    result = model_union(json_objects)
+
+    print(json.dumps(result, indent=2))
+
+if __name__ == "__main__":
+    main()

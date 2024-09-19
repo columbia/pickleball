0d12815c25d71c13860b817113c639e98c5bc803
Andreas Kellas <andreas.kellas@columbia.edu>
2024-09-19 17:17:07 -0400

    Update simple-inheritance test for near correctness

--- analyze/tests/simple-inheritance/src/model.py
+++ analyze/tests/simple-inheritance/src/model.py
@@ -1,5 +1,5 @@
 # SpecializedModel:
-# - Allowed globals: {SpecializedModel, Optimizer, SGD, Baz}
+# - Allowed globals: {SpecializedModel, Optimizer, SGD, Baz, Foo}
 # - Allowed callables: {create_tensor}
 
 
--- analyze/tests/simple-inheritance/src/pickledump.py
+++ analyze/tests/simple-inheritance/src/pickledump.py
@@ -10,7 +10,14 @@ if __name__ == "__main__":
     baz = model.Baz()
 
     model_obj = model.SpecializedModel("model-name", optimizer, tensor, baz, "special-name")
-    model_obj.bar = model.Bar()
 
     with open("model.pkl", "wb") as fd:
         pickle.dump(model_obj, fd)
+
+    optimizer2 = model.Optimizer()
+    tensor2 = model.Tensor("tensor-name", [1,2,3,4])
+    baz2 = model.Baz()
+
+    model_obj2 = model.SpecializedModel("model-name2", optimizer2, tensor2, baz2, "special-name2")
+    with open("model2.pkl", "wb") as fd:
+        pickle.dump(model_obj2, fd)

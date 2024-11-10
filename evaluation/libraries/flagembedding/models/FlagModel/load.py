8e451041761c8e8b31c9e95d2a9d75d222709f46
phli <lipenghui315@gmail.com>
2024-11-10 22:56:32 +0000

    flagembeddding

--- evaluation/libraries/flagembedding/models/FlagModel/load.py
+++ evaluation/libraries/flagembedding/models/FlagModel/load.py
@@ -1,19 +1,45 @@
 from FlagEmbedding import FlagModel
+import argparse
 
-sentences_1 = ["样例数据-1", "样例数据-2"]
-sentences_2 = ["样例数据-3", "样例数据-4"]
-model = FlagModel('BAAI/bge-large-zh-v1.5',
-                  query_instruction_for_retrieval="为这个句子生成表示以用于检索相关文章：",
-                  use_fp16=True) # Setting use_fp16 to True speeds up computation with a slight performance degradation
-embeddings_1 = model.encode(sentences_1)
-embeddings_2 = model.encode(sentences_2)
-similarity = embeddings_1 @ embeddings_2.T
-print(similarity)
 
-# for s2p(short query to long passage) retrieval task, suggest to use encode_queries() which will automatically add the instruction to each query
-# corpus in retrieval task can still use encode() or encode_corpus(), since they don't need instruction
-queries = ['query_1', 'query_2']
-passages = ["样例文档-1", "样例文档-2"]
-q_embeddings = model.encode_queries(queries)
-p_embeddings = model.encode(passages)
-scores = q_embeddings @ p_embeddings.T
+
+def load_model(model_path, test=""):
+    try:
+        sentences = [test]
+        model = FlagModel(model_path)
+
+        embeddings = model.encode(sentences)
+        print(embeddings)
+        #scores = embeddings @ embeddings.T
+        #print(f"Similarity scores:\n{scores}")
+
+    except Exception as e:
+        print(f"\033[91mFAILED in {model_path}\033[0m")
+        print(e)
+    else:
+        print(f"\033[92mSUCCEEDED in {model_path}\033[0m")
+
+
+
+if __name__ == "__main__":
+    """Main function. Collect command line arguments and begin"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model-path",
+        help=(
+            "Path to the model (parent of pytorch_model.bin)."
+        ),
+    )
+    parser.add_argument(
+        "--test",
+        default="The happy man has been eating at the diner",
+        help=(
+            "test input for the model (current string type)"
+        ),
+    )
+    args = parser.parse_args()
+    if not args.model_path:
+        print("ERROR: need to specify model path (parent of pytorch_model.bin file) and test input")
+        exit(1)
+
+    load_model(args.model_path, args.test)

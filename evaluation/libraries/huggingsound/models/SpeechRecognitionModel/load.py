d194d811f0c3d446486358e93bd07d3b08cf189e
phli <lipenghui315@gmail.com>
2024-11-10 23:33:11 +0000

    huggingsound, load locally

--- evaluation/libraries/huggingsound/models/SpeechRecognitionModel/load.py
+++ evaluation/libraries/huggingsound/models/SpeechRecognitionModel/load.py
@@ -1,6 +1,33 @@
+import argparse
 from huggingsound import SpeechRecognitionModel
 
-model = SpeechRecognitionModel("jonatasgrosman/wav2vec2-large-xlsr-53-english")
-audio_paths = ["/path/to/file.mp3", "/path/to/another_file.wav"]
+def load_model(model_path):
 
-transcriptions = model.transcribe(audio_paths)
+    try:
+        model = SpeechRecognitionModel(model_path)
+        audio_paths = ["test.wav"]
+        transcriptions = model.transcribe(audio_paths)
+        print(transcriptions)
+
+    except Exception as e:
+        print(f"\033[91mFAILED in {model_path}\033[0m")
+        print(e)
+    else:
+        print(f"\033[92mSUCCEEDED in {model_path}\033[0m")
+
+
+if __name__ == "__main__":
+    """Main function. Collect command line arguments and begin"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model-path",
+        help=(
+            "Path to the model (parent of pytorch_model.bin)."
+        ),
+    )
+    args = parser.parse_args()
+    if not args.model_path:
+        print("ERROR: need to specify model path (parent of pytorch_model.bin file). test.wav is specified.")
+        exit(1)
+
+    load_model(args.model_path)

57211f808cbc0ce45585ac558611a7f5234d7967
phli <lipenghui315@gmail.com>
2024-11-10 22:47:09 +0000

    sentence-transformer

--- evaluation/libraries/sentence-transformers/models/SentenceTransformer/load.py
+++ evaluation/libraries/sentence-transformers/models/SentenceTransformer/load.py
@@ -1,12 +1,41 @@
-import logging
-import sys
-
 from sentence_transformers import SentenceTransformer
+import argparse
 
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+def load_model(model_path, test=""):
+    try:
+        #sentences = ["안녕하세요?", "한국어 문장 임베딩을 위한 버트 모델입니다."]
+        sentences = [test]
 
-sentences = ["안녕하세요?", "한국어 문장 임베딩을 위한 버트 모델입니다."]
+        model = SentenceTransformer(model_path)
+        embeddings = model.encode(sentences)
+        print(embeddings)
 
-model = SentenceTransformer('./ko-sroberta-multitask/')
-embeddings = model.encode(sentences)
-print(embeddings)
+    except Exception as e:
+        print(f"\033[91mFAILED in {model_path}\033[0m")
+    else:
+        print(f"\033[92mSUCCEEDED in {model_path}\033[0m")
+
+
+
+if __name__ == "__main__":
+    """Main function. Collect command line arguments and begin"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model-path",
+        help=(
+            "Path to the model (parent of pytorch_model.bin)."
+        ),
+    )
+    parser.add_argument(
+        "--test",
+        default="The happy man has been eating at the diner",
+        help=(
+            "test input for the model (current string type)"
+        ),
+    )
+    args = parser.parse_args()
+    if not args.model_path:
+        print("ERROR: need to specify model path (parent of pytorch_model.bin file) and test input")
+        exit(1)
+
+    load_model(args.model_path, args.test)

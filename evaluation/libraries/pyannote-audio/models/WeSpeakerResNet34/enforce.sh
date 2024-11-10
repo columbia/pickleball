538cdeabc5455d548432b66d4971a8d11a704c88
Andreas Kellas <andreas.kellas@columbia.edu>
2024-11-09 22:57:52 -0500

    Fix path in pyannote enforce.sh script

--- evaluation/libraries/pyannote-audio/models/WeSpeakerResNet34/enforce.sh
+++ evaluation/libraries/pyannote-audio/models/WeSpeakerResNet34/enforce.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker run --rm -it \
-    -v $(pwd)/policy.json:/root/policies.json \
+    -v $(pwd)/policy.json:/root/policies/policy.json \
     -v $(pwd)/load.py:/load-model/load.py \
     pickleball-pyannote /bin/bash

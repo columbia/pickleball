c7d370c3436362d9326eb1e3f1c2cf0d7e5a6b95
Andreas Kellas <andreas.kellas@columbia.edu>
2025-02-21 17:49:30 -0500

    Refactor library fetch script

--- evaluation/setup/fetch.sh
+++ evaluation/setup/fetch.sh
@@ -7,94 +7,40 @@ SETUP_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 EVALUATION_DIR="$SETUP_DIR/../"
 LIBRARIES_DIR="$EVALUATION_DIR/libraries"
 
-# conch
-cd $LIBRARIES_DIR
-git clone --recurse-submodules https://github.com/mahmoodlab/CONCH.git
-cd CONCH
-git checkout 02d6ac5
+clone_and_checkout() {
+    local repo_url=$1
+    local repo_name=$2
+    local commit_hash=$3
+    local apply_diff=$4
 
-# flagembedding
-cd $LIBRARIES_DIR
-git clone --recurse-submodules https://github.com/FlagOpen/FlagEmbedding.git
-cd FlagEmbedding
-# The original evaluation used the 2bfc922 commit, but the FlagEmbedding
+    cd "$LIBRARIES_DIR" || return 1
+    git clone --recurse-submodules "$repo_url" || return 1
+    cd "$repo_name" || return 1
+    git checkout "$commit_hash" || return 1
+
+    # Run extra command if provided
+    if [[ -n "$apply_diff" ]]; then
+	echo "applying diff"
+        git apply $SETUP_DIR/$apply_diff
+    fi
+}
+
+# Checkout repository URL, cd into directory, checkout commit, [optionally apply diff]
+clone_and_checkout https://github.com/mahmoodlab/CONCH.git CONCH 02d6ac5
+# The original FlagEmbedding evaluation used the 2bfc922 commit, but the
 # library has been significantly rewritten and the git commit history has been
 # overwritten. We have identified the bf6b649 commit as being close to the
 # library state when we did our evaluation.
-#git checkout 2bfc922
-git checkout bf6b649
-
-# flair
-cd $LIBRARIES_DIR
-git clone --recurse-submodules https://github.com/flairNLP/flair.git
-cd flair
-git checkout c674212
-git apply $SETUP_DIR/flair.diff
-
-# gliner
-cd $LIBRARIES_DIR
-git clone --recurse-submodules https://github.com/urchade/GLiNER.git
-cd GLiNER
-git checkout 1169120
-
-# huggingsound
-cd $LIBRARIES_DIR
-git clone --recurse-submodules https://github.com/jonatasgrosman/huggingsound.git
-cd huggingsound
-git checkout 50e9fba
-
-# languagebind
-cd $LIBRARIES_DIR
-git clone --recurse-submodules https://github.com/PKU-YuanGroup/LanguageBind.git
-cd LanguageBind
-git checkout 7070c53
-
-# melotts
-cd $LIBRARIES_DIR
-git clone --recurse-submodules https://github.com/myshell-ai/MeloTTS.git
-cd MeloTTS
-git checkout 5b53848
-
-# parrot
-cd $LIBRARIES_DIR
-git clone --recurse-submodules https://github.com/PrithivirajDamodaran/Parrot_Paraphraser.git
-cd Parrot_Paraphraser
-git checkout 03084c5
-
-# pyannote
-cd $LIBRARIES_DIR
-git clone --recurse-submodules https://github.com/pyannote/pyannote-audio.git
-cd pyannote-audio
-git checkout 0ea4c02
-git apply $SETUP_DIR/pyannote.diff
-
-# pysentimiento
-cd $LIBRARIES_DIR
-git clone --recurse-submodules https://github.com/pysentimiento/pysentimiento.git
-cd pysentimiento
-git checkout 60822ac
-
-# sentence-transformers
-cd $LIBRARIES_DIR
-git clone --recurse-submodules https://github.com/UKPLab/sentence-transformers.git
-cd sentence-transformers
-git checkout a1db32d
-git apply $SETUP_DIR/sentence-transformers.diff
-
-# superimage
-cd $LIBRARIES_DIR
-git clone --recurse-submodules https://github.com/eugenesiow/super-image.git
-cd super-image
-git checkout 50439ea
-
-# tner
-cd $LIBRARIES_DIR
-git clone --recurse-submodules https://github.com/asahi417/tner.git
-cd tner
-git checkout 7730a62
-
-# tweetnlp
-cd $LIBRARIES_DIR
-git clone --recurse-submodules https://github.com/cardiffnlp/tweetnlp.git
-cd tweetnlp
-git checkout 68b08c8
+clone_and_checkout https://github.com/FlagOpen/FlagEmbedding.git FlagEmbedding bf6b649
+clone_and_checkout https://github.com/flairNLP/flair.git flair c674212 flair.diff
+clone_and_checkout https://github.com/urchade/GLiNER.git GLiNER 1169120
+clone_and_checkout https://github.com/jonatasgrosman/huggingsound.git huggingsound 50e9fba
+clone_and_checkout https://github.com/PKU-YuanGroup/LanguageBind.git LanguageBind 7070c53
+clone_and_checkout https://github.com/myshell-ai/MeloTTS.git MeloTTS 5b53848
+clone_and_checkout https://github.com/PrithivirajDamodaran/Parrot_Paraphraser.git Parrot_Paraphraser 03084c5
+clone_and_checkout https://github.com/pyannote/pyannote-audio.git pyannote-audio 0ea4c02 pyannote.diff
+clone_and_checkout https://github.com/pysentimiento/pysentimiento.git pysentimiento 60822ac
+clone_and_checkout https://github.com/UKPLab/sentence-transformers.git sentence-transformers a1db32d sentence-transformers.diff
+clone_and_checkout https://github.com/eugenesiow/super-image.git super-image 50439ea
+clone_and_checkout https://github.com/asahi417/tner.git tner 7730a62
+clone_and_checkout https://github.com/cardiffnlp/tweetnlp.git tweetnlp 68b08c8

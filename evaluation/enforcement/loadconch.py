c4e6a0c7ce27991a4f7692ea7596a52290e83884
Neophytos Christou <neophytos_christou@brown.edu>
2025-02-24 09:55:44 -0500

    Add conch

--- evaluation/enforcement/loadconch.py
+++ evaluation/enforcement/loadconch.py
@@ -1,16 +1,20 @@
-import torch
 import argparse
 from pathlib import Path
-from PIL import Image
+
+import torch
 from conch.open_clip_custom import create_model_from_pretrained
-from pklballcheck import verify_loader_was_used
+from PIL import Image
+from pklballcheck import collect_attr_stats, verify_loader_was_used
 
 DIR = Path(__file__).parent
+
 
 def load_model(model_path, test="") -> bool:
     try:
 
-        model, preprocess = create_model_from_pretrained("conch_ViT-B-16", checkpoint_path=model_path)
+        model, preprocess = create_model_from_pretrained(
+            "conch_ViT-B-16", checkpoint_path=model_path
+        )
 
         image = Image.open(str(DIR / "conch-test" / "test.jpg"))
         image = preprocess(image).unsqueeze(0)
@@ -25,7 +29,8 @@ def load_model(model_path, test="") -> bool:
     else:
         print(f"\033[92mSUCCEEDED in {model_path}\033[0m")
         return True
-
+    finally:
+        collect_attr_stats(model)
 
 
 if __name__ == "__main__":
@@ -33,20 +38,18 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--model-path",
-        help=(
-            "Path to the model (pytorch_model.bin)."
-        ),
+        help=("Path to the model (pytorch_model.bin)."),
     )
     parser.add_argument(
         "--test",
         default="The happy man has been eating at the diner",
-        help=(
-            "test input for the model (current string type)"
-        ),
+        help=("test input for the model (current string type)"),
     )
     args = parser.parse_args()
     if not args.model_path:
-        print("ERROR: need to specify model path (pytorch_model.bin file) and test img input")
+        print(
+            "ERROR: need to specify model path (pytorch_model.bin file) and test img input"
+        )
         exit(1)
 
     load_model(args.model_path, args.test)

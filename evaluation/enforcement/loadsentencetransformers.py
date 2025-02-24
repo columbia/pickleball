4ddd1f585a5f6ca31dcb4143ecff09120ea427f5
Neophytos Christou <neophytos_christou@brown.edu>
2025-02-23 22:29:07 -0500

    Add sentencetransformers

--- evaluation/enforcement/loadsentencetransformers.py
+++ evaluation/enforcement/loadsentencetransformers.py
@@ -1,11 +1,11 @@
 import argparse
 from pathlib import Path
 
+from pklballcheck import collect_attr_stats, verify_loader_was_used
 from sentence_transformers import SentenceTransformer
 
-from pklballcheck import verify_loader_was_used
+TEST = ("The happy man has been eating at the diner",)
 
-TEST = "The happy man has been eating at the diner",
 
 def load_model(model_path, test=TEST) -> bool:
 
@@ -24,7 +24,8 @@ def load_model(model_path, test=TEST) -> bool:
     else:
         print(f"\033[92mSUCCEEDED in {model_path}\033[0m")
         return True
-
+    finally:
+        collect_attr_stats(model)
 
 
 if __name__ == "__main__":
@@ -32,20 +33,18 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--model-path",
-        help=(
-            "Path to the model (pytorch_model.bin)."
-        ),
+        help=("Path to the model (pytorch_model.bin)."),
     )
     parser.add_argument(
         "--test",
         default="The happy man has been eating at the diner",
-        help=(
-            "test input for the model (current string type)"
-        ),
+        help=("test input for the model (current string type)"),
     )
     args = parser.parse_args()
     if not args.model_path:
-        print("ERROR: need to specify model path (parent of pytorch_model.bin file) and test input")
+        print(
+            "ERROR: need to specify model path (parent of pytorch_model.bin file) and test input"
+        )
         exit(1)
 
     load_model(args.model_path, args.test)

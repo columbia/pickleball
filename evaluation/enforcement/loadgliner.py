76d15322201be6283fca0269b05b37140e048e52
Neophytos Christou <neophytos_christou@brown.edu>
2025-02-23 20:44:01 -0500

    Fix paths and add gliner

--- evaluation/enforcement/loadgliner.py
+++ evaluation/enforcement/loadgliner.py
@@ -1,15 +1,27 @@
 import argparse
-from gliner import GLiNER
 from pathlib import Path
 
-#model = GLiNER.from_pretrained("EmergentMethods/gliner_medium_news-v2.1")
-#model = GLiNER.from_pretrained("/data/no-malicious/gliner/GliNER/DeepMount00-GLiNER_ITA_BASE",
+from gliner import GLiNER
+from pklballcheck import collect_attr_stats, verify_loader_was_used
+
+# model = GLiNER.from_pretrained("EmergentMethods/gliner_medium_news-v2.1")
+# model = GLiNER.from_pretrained("/data/no-malicious/gliner/GliNER/DeepMount00-GLiNER_ITA_BASE",
 
 TEXT = """
 The Chihuahua State Public Security Secretariat (SSPE) arrested 35-year-old Salomón C. T. in Ciudad Juárez, found in possession of a stolen vehicle, a white GMC Yukon, which was reported stolen in the city's streets. The arrest was made by intelligence and police analysis personnel during an investigation in the border city. The arrest is related to a previous detention on February 6, which involved armed men in a private vehicle. The detainee and the vehicle were turned over to the Chihuahua State Attorney General's Office for further investigation into the case.
 """
 
-LABELS = ["person", "location", "date", "event", "facility", "vehicle", "number", "organization"]
+LABELS = [
+    "person",
+    "location",
+    "date",
+    "event",
+    "facility",
+    "vehicle",
+    "number",
+    "organization",
+]
+
 
 def load_model(model_path, text=TEXT, labels=LABELS) -> bool:
     try:
@@ -22,7 +34,7 @@ def load_model(model_path, text=TEXT, labels=LABELS) -> bool:
 
         for entity in entities:
             print(entity["text"], "=>", entity["label"])
-        
+
     except Exception as e:
         print(f"\033[91mFAILED in {model_path}\033[0m: {e}")
         print(e)
@@ -30,4 +42,28 @@ def load_model(model_path, text=TEXT, labels=LABELS) -> bool:
     else:
         print(f"\033[92mSUCCEEDED in {model_path}\033[0m")
         return True
+    finally:
+        collect_attr_stats(model)
 
+
+if __name__ == "__main__":
+    """Main function. Collect command line arguments and begin"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model-path",
+        help=("Path to the model (pytorch_model.bin)."),
+    )
+    parser.add_argument(
+        "--test",
+        default="The happy man has been eating at the diner",
+        help=("test input for the model (current string type)"),
+    )
+    args = parser.parse_args()
+    if not args.model_path:
+        print(
+            "ERROR: need to specify model path (pytorch_model.bin file) and (optional) test input"
+        )
+        exit(1)
+
+    load_model(args.model_path, args.test)
+    verify_loader_was_used()

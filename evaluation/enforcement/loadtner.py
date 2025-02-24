500cb345c5331029f3d7ed245f14be0502986d99
Neophytos Christou <neophytos_christou@brown.edu>
2025-02-23 22:44:19 -0500

    Add tner

--- evaluation/enforcement/loadtner.py
+++ evaluation/enforcement/loadtner.py
@@ -1,17 +1,23 @@
 import argparse
 from pathlib import Path
 
+from pklballcheck import collect_attr_stats, verify_loader_was_used
 from tner import TransformersNER
 
 TEST = "The happy man has been eating at the diner"
 
+
 def load_model(model_path, test=TEST) -> bool:
 
     model_dir = Path(model_path).parent
-    print(f'model_dir: {model_dir}')
+    print(f"model_dir: {model_dir}")
     try:
         model = TransformersNER(str(model_dir))
-        print(model.predict(["Jacob Collier is a Grammy awarded English artist from London"]))
+        print(
+            model.predict(
+                ["Jacob Collier is a Grammy awarded English artist from London"]
+            )
+        )
     except Exception as e:
         print(f"\033[91mFAILED in {model_path}\033[0m")
         print(e)
@@ -19,6 +25,8 @@ def load_model(model_path, test=TEST) -> bool:
     else:
         print(f"\033[92mSUCCEEDED in {model_path}\033[0m")
         return True
+    finally:
+        collect_attr_stats(model)
 
 
 if __name__ == "__main__":
@@ -26,20 +34,18 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--model-path",
-        help=(
-            "Path to the model (pytorch_model.bin)."
-        ),
+        help=("Path to the model (pytorch_model.bin)."),
     )
     parser.add_argument(
         "--test",
         default="The happy man has been eating at the diner",
-        help=(
-            "test input for the model (current string type)"
-        ),
+        help=("test input for the model (current string type)"),
     )
     args = parser.parse_args()
     if not args.model_path:
-        print("ERROR: need to specify model path (parent directory of pytorch_model.bin file) and (optional) test input")
+        print(
+            "ERROR: need to specify model path (parent directory of pytorch_model.bin file) and (optional) test input"
+        )
         exit(1)
 
     load_model(args.model_path, args.test)

93dd97370d6134bd8bd26fedba20322a4e057a4f
Andreas Kellas <andreas.kellas@columbia.edu>
2025-02-23 19:08:13 -0500

    Fix gliner load evaluation

--- evaluation/enforcement/loadgliner.py
+++ evaluation/enforcement/loadgliner.py
@@ -2,6 +2,8 @@ import argparse
 from gliner import GLiNER
 from pathlib import Path
 
+from pklballcheck import verify_loader_was_used
+
 #model = GLiNER.from_pretrained("EmergentMethods/gliner_medium_news-v2.1")
 #model = GLiNER.from_pretrained("/data/no-malicious/gliner/GliNER/DeepMount00-GLiNER_ITA_BASE",
 
@@ -31,3 +33,29 @@ def load_model(model_path, text=TEXT, labels=LABELS) -> bool:
         print(f"\033[92mSUCCEEDED in {model_path}\033[0m")
         return True
 
+if __name__ == "__main__":
+    """Main function. Collect command line arguments and begin"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model-path",
+        help=(
+            "Path to the model directory (pytorch_model.bin)."
+        ),
+    )
+    parser.add_argument(
+        "--test",
+        default="The happy man has been eating at the diner",
+        help=(
+            "test input for the model (current string type)"
+        ),
+    )
+    args = parser.parse_args()
+    if not args.model_path:
+        print("ERROR: need to specify model directory (pytorch_model.bin) and (optional) test input")
+        exit(1)
+
+    load_model(args.model_path, args.test)
+    if verify_loader_was_used():
+        print("Loader used")
+    else:
+        print("Error: Loader not used")

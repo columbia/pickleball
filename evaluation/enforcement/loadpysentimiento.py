e03e590b4dbd669796aa93406a46baa46aaeaa75
Neophytos Christou <neophytos_christou@brown.edu>
2025-02-23 22:22:49 -0500

    Add pysentimiento

--- evaluation/enforcement/loadpysentimiento.py
+++ evaluation/enforcement/loadpysentimiento.py
@@ -1,7 +1,9 @@
 import argparse
 from pathlib import Path
 
+from pklballcheck import collect_attr_stats, verify_loader_was_used
 from pysentimiento import create_analyzer
+
 
 def load_model(model_path, test="") -> bool:
 
@@ -20,7 +22,8 @@ def load_model(model_path, test="") -> bool:
     else:
         print(f"\033[92mSUCCEEDED in {model_path}\033[0m")
         return True
-
+    finally:
+        collect_attr_stats(analyzer)
 
 
 if __name__ == "__main__":
@@ -28,20 +31,18 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--model-path",
-        help=(
-            "Path to the model directory (parent/of/pytorch_model.bin)."
-        ),
+        help=("Path to the model directory (parent/of/pytorch_model.bin)."),
     )
     parser.add_argument(
         "--test",
         default="The happy man has been eating at the diner",
-        help=(
-            "test input for the model (current string type)"
-        ),
+        help=("test input for the model (current string type)"),
     )
     args = parser.parse_args()
     if not args.model_path:
-        print("ERROR: need to specify model directory (parent/of/pytorch_model.bin) and (optional) test input")
+        print(
+            "ERROR: need to specify model directory (parent/of/pytorch_model.bin) and (optional) test input"
+        )
         exit(1)
 
     load_model(args.model_path, args.test)

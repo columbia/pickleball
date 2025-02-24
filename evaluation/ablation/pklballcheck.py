7500fb93a13b0aafd8276369162ae1e730f003e1
Andreas Kellas <andreas.kellas@columbia.edu>
2025-02-23 20:55:56 -0500

    Change pklballcheck from symlink to hardfile

--- evaluation/ablation/pklballcheck.py
+++ evaluation/ablation/pklballcheck.py
@@ -1,1 +1,12 @@
-../../enforce/pklballcheck.py
+from pathlib import Path
+
+PLACEHOLDER_FILE_PATH = Path("/root/.loader_used")
+
+
+def verify_loader_was_used() -> bool:
+    if PLACEHOLDER_FILE_PATH.is_file():
+        PLACEHOLDER_FILE_PATH.unlink()
+        return True
+    else:
+        #raise Exception("PICKLEBALL loader was not used to load the model!")
+        return False

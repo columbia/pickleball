a05fcb20b61f81a09efe04ea89ab783be884792a
Andreas Kellas <andreas.kellas@columbia.edu>
2025-04-10 17:18:15 -0400

    Minor update to loadyolov11.py

--- evaluation/enforcement/loadyolov11.py
+++ evaluation/enforcement/loadyolov11.py
@@ -25,7 +25,6 @@ def load_model(model_path, test=IMG_PATH) -> bool:
         return False
     else:
         print(f"\033[92mSUCCEEDED in {model_path}\033[0m")
-        print(type(model))
         #collect_attr_stats(model)
         return True
 

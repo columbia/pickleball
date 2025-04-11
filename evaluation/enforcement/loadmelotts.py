8d5a5f7ca980ffbffc6d856f9100780d025f8b3d
Neophytos Christou <neophytos_christou@brown.edu>
2025-04-11 18:25:23 -0400

    Melotts output

--- evaluation/enforcement/loadmelotts.py
+++ evaluation/enforcement/loadmelotts.py
@@ -2,7 +2,8 @@ import argparse
 from pathlib import Path
 
 from melo.api import TTS
-from pklballcheck import collect_attr_stats, verify_loader_was_used
+
+# from pklballcheck import collect_attr_stats, verify_loader_was_used
 
 # https://github.com/myshell-ai/MeloTTS/blob/5b538481e24e0d578955be32a95d88fcbde26dc8/melo/download_utils.py#L33C1-L33C19
 LANG_MAP = {
@@ -20,7 +21,7 @@ LANG_MAP = {
 TEXT = "The happy man has been eating at the diner"
 
 
-def load_model(model_path: str, text=TEXT) -> bool:
+def load_model(model_path: str, text=TEXT) -> tuple[bool, str]:
 
     model_dir = Path(model_path).parent
     config_path = model_dir / "config.json"
@@ -43,8 +44,12 @@ def load_model(model_path: str, text=TEXT) -> bool:
         speaker_ids = model.hps.data.spk2id
         speakers = list(speaker_ids.keys())
         output_path = "test.wav"
+        output = ""
         for speaker in speakers:
-            model.tts_to_file(text, speaker_ids[speaker], output_path, speed=1.0)
+            # model.tts_to_file(text, speaker_ids[speaker], output_path, speed=1.0)
+            output = model.tts_to_file(
+                text, speaker_ids[speaker], output_path=None, speed=1.0
+            )
 
             # just one is enough
             break
@@ -52,11 +57,11 @@ def load_model(model_path: str, text=TEXT) -> bool:
     except Exception as e:
         print(f"\033[91mFAILED in {model_path}\033[0m")
         print(e)
-        return False
+        return False, ""
     else:
         print(f"\033[92mSUCCEEDED in {model_path}\033[0m")
         collect_attr_stats(model)
-        return True
+        return True, output
 
 
 if __name__ == "__main__":
@@ -80,5 +85,5 @@ if __name__ == "__main__":
         print("ERROR: need to specify model path (checkpoint.pth)")
         exit(1)
 
-    load_model(args.model_path, args.test)
-    verify_loader_was_used()
+    is_success, output = load_model(args.model_path, args.test)
+    # verify_loader_was_used()

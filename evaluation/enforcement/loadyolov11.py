cc7367e2b62fb6b2e43d69b6543f0a7713155640
Neophytos Christou <neophytos_christou@brown.edu>
2025-04-12 12:18:56 -0400

    Validation for yolov11

--- evaluation/enforcement/loadyolov11.py
+++ evaluation/enforcement/loadyolov11.py
@@ -1,23 +1,43 @@
 import argparse
+import os
 from pathlib import Path
 
 import ultralytics
 
-#from pklballcheck import collect_attr_stats, verify_loader_was_used
+# from pklballcheck import collect_attr_stats, verify_loader_was_used
 from pklballcheck import verify_loader_was_used
 
-#IMG_PATH = 'https://github.com/ultralytics/yolov5/raw/master/data/images/zidane.jpg'
+# IMG_PATH = 'https://github.com/ultralytics/yolov5/raw/master/data/images/zidane.jpg'
 DIR = Path(__file__).parent
-IMG_PATH = DIR / Path('test-yolov11') / Path('zidane.jpg')
+IMG_PATH = DIR / Path("test-yolov11") / Path("zidane.jpg")
+VALIDATION_DIR = DIR / Path("test2017")
 
 
-def load_model(model_path, test=IMG_PATH) -> bool:
+def validate_model(model_path):
+
+    validation_files = os.listdir(VALIDATION_DIR)
+
+    try:
+        model = ultralytics.YOLO(model_path)
+
+        for file in validation_files:
+            results = model.predict(VALIDATION_DIR / file, save=False)
+            print(results)
+
+    except Exception as e:
+        print(f"\033[91mFAILED in {model_path}\033[0m")
+        print(e)
+    else:
+        print(f"\033[92mSUCCEEDED in {model_path}\033[0m")
+
+
+def load_model(model_path, test=IMG_PATH) -> tuple[bool, str]:
 
     try:
         model = ultralytics.YOLO(model_path)
         results = model.predict(test, save=False)
 
-        print(f'results: {results}')
+        print(f"results: {results}")
 
     except Exception as e:
         print(f"\033[91mFAILED in {model_path}\033[0m")
@@ -25,7 +45,7 @@ def load_model(model_path, test=IMG_PATH) -> bool:
         return False, ""
     else:
         print(f"\033[92mSUCCEEDED in {model_path}\033[0m")
-        #collect_attr_stats(model)
+        # collect_attr_stats(model)
         return True, str(results)
 
 
@@ -41,11 +61,17 @@ if __name__ == "__main__":
         default=IMG_PATH,
         help=("Path to an image to test."),
     )
+    parser.add_argument("--validate", action="store_true")
     args = parser.parse_args()
     if not args.model_path:
-        print("ERROR: need to specify model path (.pt file) and (optional) "
-              "test input")
+        print(
+            "ERROR: need to specify model path (.pt file) and (optional) " "test input"
+        )
         exit(1)
 
-    load_model(args.model_path, args.test)
+    if args.validate:
+        validate_model(args.model_path)
+    else:
+        is_success, output = load_model(args.model_path)
+        print(output)
     verify_loader_was_used()

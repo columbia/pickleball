c7b4968cb9e5981af440179c6e9e127ee5338aff
Neophytos Christou <neophytos_christou@brown.edu>
2025-04-12 10:10:02 -0400

    Add validation code in enforcement dir

--- evaluation/enforcement/loadflair.py
+++ evaluation/enforcement/loadflair.py
@@ -1,10 +1,53 @@
 import argparse
 
+import flair.datasets
 from flair.data import Sentence
 from flair.models import SequenceTagger
 from pklballcheck import collect_attr_stats, verify_loader_was_used
 
+corpus_map = {
+    "flair-ner-english": flair.datasets.NER_ENGLISH_PERSON,
+    "flair-ner-english-fast": flair.datasets.NER_ENGLISH_PERSON,
+    "flair-ner-english-large": flair.datasets.NER_ENGLISH_PERSON,
+    "flair-chunk-english-fast": flair.datasets.NER_ENGLISH_PERSON,
+    "flair-ner-english-ontonotes-large": flair.datasets.NER_ENGLISH_PERSON,
+    "flair-ner-english-ontonotes-fast": flair.datasets.NER_ENGLISH_PERSON,
+    "flair-ner-spanish-large": flair.datasets.UP_SPANISH,
+    "flair-ner-dutch-large": flair.datasets.UD_DUTCH,
+}
+
+
 TEST = "The experienced professor from Harvard University quickly analyzed the ancient manuscript while drinking coffee in New York last Sunday."
+
+
+def validate_model(model_path):
+
+    model_name = model_path.split("/")[-2]
+    corpus = corpus_map[model_name]()
+
+    sentences = corpus.get_all_sentences()
+    print(f"Total sentences: {len(sentences)}")
+    try:
+        model = SequenceTagger.load(model_path)
+        for sentence in sentences:
+
+            model.predict(sentence)
+
+            entities = sentence.get_spans()
+            output = "\n".join([str(entity) for entity in entities])
+
+            # If we are running a POS model the spans will be empty and the tags
+            # will be incorporated in the sentence
+            if len(output) == 0:
+                output = sentence
+
+            print(output)
+
+    except Exception as e:
+        print(f"\033[91mFAILED in {model_name}\033[0m")
+        print(e)
+    else:
+        print(f"\033[92mSUCCEEDED in {model_name}\033[0m")
 
 
 def load_model(model_path, test=TEST) -> tuple[bool, str]:
@@ -44,6 +87,7 @@ if __name__ == "__main__":
         default="The happy man has been eating at the diner",
         help=("test input for the model (current string type)"),
     )
+    parser.add_argument("--validate", action="store_true")
     args = parser.parse_args()
     if not args.model_path:
         print(
@@ -51,6 +95,9 @@ if __name__ == "__main__":
         )
         exit(1)
 
-    is_success, output = load_model(args.model_path)
-    print(output)
+    if args.validate:
+        validate_model()
+    else:
+        is_success, output = load_model(args.model_path)
+        print(output)
     verify_loader_was_used()
